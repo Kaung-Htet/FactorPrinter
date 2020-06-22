25d84a974ea9700ce5971db12708c9cc41fba8b9
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-06-22 22:10:45 +0700

    Add printFactors method

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        FactorPrinter.printFactors(6);
+        FactorPrinter.printFactors(32);
+        FactorPrinter.printFactors(10);
+        FactorPrinter.printFactors(-1);
+    }
 }
+
+//* printFactors(6); → should print 1 2 3 6
+//
+//* printFactors(32); → should print 1 2 4 8 16 32
+//
+//* printFactors(10); → should print 1 2 5 10
+//
+//* printFactors(-1); → should print "Invalid Value" since number is < 1
